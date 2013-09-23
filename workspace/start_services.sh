54e66ccf37d0866576689e4f29412b38ba35e7e8
edam <development@footballpools.com>
2013-09-23 16:57:59 +0100

    #0 PC : Update start services script

--- workspace/start_services.sh
+++ workspace/start_services.sh
@@ -1,24 +1,25 @@
-pkill -9 ruby
+pkill -9 ruby.
 ps aux | grep 'unicorn' | awk '{print $2}' | xargs kill -9
 
 redis-cli flushdb
-cd competition_management && bundle install && bundle exec rake db:reset && bundle exec rails s -p3006 &
+
 #cd turnstile && bundle install && bundle exec rake db:migrate && rails s &
 #cd customerservice && bundle install && bundle exec rake db:migrate && rails s -p3001 &
 cd customerservice && ./script/run_for_test_integration.sh &
-cd entry_service && bundle install && bundle exec rake db:reset && bundle exec rake db:migrate && bundle exec rake db:migrate RAILS_ENV=test && bundle exec rails s -p3007 &
-cd legacy_service && bundle install && bundle exec rake db:reset && bundle exec rake db:migrate && bundle exec rake db:migrate RAILS_ENV=test && bundle exec rails s -p3002 &
-
-#cd orders_service && bundle install && bundle exec rake db:reset && bundle exec rake db:migrate && bundle exec rake db:migrate RAILS_ENV=test && bundle exec rails s -p3004 &
-cd orders_service && bundle install && bundle exec rake db:reset && bundle exec rake db:migrate && bundle exec rake db:migrate RAILS_ENV=test && bundle exec unicorn_rails -c unicorn.conf.minimal.rb -D &
-
+#cd legacy_service  && bundle install && bundle exec rake db:reset && bundle exec rake db:migrate && bundle exec rake db:migrate RAILS_ENV=test && bundle exec rails s -p3002 &
+cd orders_service  && bundle install && bundle exec rake db:reset && bundle exec rake db:migrate && bundle exec rake db:migrate RAILS_ENV=test && bundle exec rails s -p3004 &
+#cd orders_service && bundle install && bundle exec rake db:reset && bundle exec rake db:migrate && bundle exec rake db:migrate RAILS_ENV=test && bundle exec unicorn_rails -c unicorn.conf.minimal.rb -D &
 cd payment_service && bundle install && bundle exec rake db:reset && bundle exec rake db:migrate && bundle exec rake db:migrate RAILS_ENV=test && bundle exec rails s -p3005 &
-cd communication_service && bundle install && bundle exec rake db:reset && bundle exec rails s -p3008 &
-
+cd competition_management && bundle install && bundle exec rake db:reset && bundle exec rails s -p3006 &
+cd entry_service && bundle install && bundle exec rake db:reset && bundle exec rake db:migrate && bundle exec rake db:migrate RAILS_ENV=test && bundle exec rails s -p3007 &
+cd communication_service && bundle exec rake db:reset && bundle exec rake db:migrate && bundle exec rake db:migrate RAILS_ENV=test && bundle exec rails s -p3008 &
+cd silverpop_mock && bundle install && bundle exec rails s -p9001 &
 
 cd orders_service && bundle exec rake resque:work QUEUE=orders_service &
+cd communication_service && bundle exec rake resque:work QUEUE=communication_service &
+cd competition_management && bundle exec rake resque:work QUEUE=competition_management &
 
-
+sleep 5
 wget http://localhost:3006
 while [ "$?" -ne "0" ]; 
   do
